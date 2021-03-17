fe868884921f8c2d2aa4ea39c7f09cef0b92f406
unknown <satvikbatra@gmail.com>
2021-03-17 13:15:56 +0530

    Level 3 authentication , hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express") ;
 const bodyParser = require("body-parser") ;
 const ejs = require("ejs") ;
 const mongoose = require("mongoose") ;
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
+
 const app = express() ;
 
 app.use(express.static("public")) ;
@@ -21,7 +22,6 @@ const userSchema = new mongoose.Schema({
 
                  //defining the fields that we want to encrypt.
                  //process.env."environment_variable_name". to access its value fromm the .env file.
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']}) ;
 
 // after this we only need to to encrypt the fields we specified.
 // the plugin will automatically encrypt passwords when we save them and automatically decrypt them when we find them.
@@ -42,7 +42,7 @@ app.post("/register",function(req,res){
 
      const newUser = new User({
        email:req.body.username,
-       password:req.body.password
+       password:md5(req.body.password)
      });
      newUser.save(function(err){
        if(err){
@@ -56,7 +56,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req,res){
 
     const username = req.body.username ;
-    const password = req.body.password ;
+    const password = md5(req.body.password) ;
 
     User.findOne({email:username},function(err,foundUser){
       if(err){
